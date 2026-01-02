1a0abb150c73a8420f77458e1f8d1cb932f0fff7
ncode <nickcode987@gmail.com>
2026-01-01 22:57:50 -0500

    Initial commit

---
+++ movies/movies/src/main/java/dev/nich4545/movies/MoviesApplication.java
@@ -0,0 +1,13 @@
+package dev.nich4545.movies;
+
+import org.springframework.boot.SpringApplication; // contains 'Run' which is required to start the Spring application
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
